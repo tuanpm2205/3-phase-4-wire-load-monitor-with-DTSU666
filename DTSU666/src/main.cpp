c5ec42484b585ebd4765cb4be1d64d0896484d5a
Phạm Minh Tuấn <phamminhtuan22072005sv@gmail.com>
2026-01-22 18:29:30 +0700

    read

--- DTSU666/src/main.cpp
+++ DTSU666/src/main.cpp
@@ -1,11 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "dtsu666.h"
+#include "ModbusMaster.h"
+
 void setup(){
-  
+    Serial.begin(9600);
+    Init_dtsu666();
 }
 
 void loop(){
-
-
-  while(1){
-
-  }
+    Serial.print("Ua:");
+    Serial.println(Read_Voltage_A());
+    Serial.print("Ub:");
+    Serial.println(Read_Voltage_B());
+    Serial.print("Uc:");
+    Serial.println(Read_Voltage_C());
 }
